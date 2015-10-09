21392fcccbd8aca364d0c1cfc8f6a8b63c160f10
Caio Oliveira <caiooliveirafarias0@gmail.com>
2015-10-09 20:47:41 -0300

    build.sh: Fix mistype
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- zip-creator/tools/kernel_flash.sh
+++ zip-creator/tools/kernel_flash.sh
@@ -2,6 +2,6 @@
 cd /tmp/
 dd if=/dev/block/mmcblk0p9 of=/tmp/boot.img
 /tmp/unpackbootimg /tmp/boot.img
-/tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline $(cat /tmp/boot.img-cmdline) --base $(cat /tmp/boot.img-base) -o /tmp/newboot.img
+/tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "$(cat /tmp/boot.img-cmdline)" --base "$(cat /tmp/boot.img-base)" -o /tmp/newboot.img
 dd if=/tmp/newboot.img of=/dev/block/mmcblk0p9
 busybox chmod 644 /system/lib/modules/*
