333162ff6e2f2f18f4993136f5f5bbde4543a3d8
Caio Oliveira <caiooliveirafarias0@gmail.com>
2015-10-29 22:22:14 -0200

    build.sh: Use APTESS
    [New] Android Prebuilt Toolchains External Script Set:
    So no more updates here when new toolchain come
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- build.sh
+++ build.sh
@@ -107,26 +107,7 @@ fi
 
 maintoolchain() {
 if [ -d ../android_prebuilt_toolchains ]; then
-	echo "1) 4.7 Google GCC"
-	echo "2) 4.8 Google GCC"
-	echo "3) 4.6.4 Linaro GCC"
-	echo "4) 4.7.4 Linaro GCC"
-	echo "5) 4.8.4 Linaro GCC"
-	echo "6) 4.9.3 Linaro GCC"
-	unset errortoolchain
-	read -p "Choice: " -n 1 -s toolchain
-	case "$toolchain" in
-		1 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.7/bin/arm-eabi-"; ToolchainCompile="GCC 4.7";;
-		2 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-eabi-4.8/bin/arm-eabi-"; ToolchainCompile="GCC 4.8";;
-		3 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-unknown-linux-gnueabi-linaro_4.6.4-2013.05/bin/arm-unknown-linux-gnueabi-"; ToolchainCompile="Linaro 4.6.4";;
-		4 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-unknown-linux-gnueabi-linaro_4.7.4-2013.12/bin/arm-unknown-linux-gnueabi-"; ToolchainCompile="Linaro 4.7.4";;
-		5 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-linux-gnueabi-linaro_4.8.4-2014.11/bin/arm-linux-gnueabi-"; ToolchainCompile="Linaro 4.8.4";;
-		6 ) export CROSS_COMPILE="../android_prebuilt_toolchains/arm-cortex-linux-gnueabi-linaro_4.9.3-2015.03/bin/arm-cortex-linux-gnueabi-"; ToolchainCompile="Linaro 4.9.3";;
-		* ) echo "$toolchain - This option is not valid"; sleep .5; errortoolchain="ON";;
-	esac
-	if ! [ "$errortoolchain" == "ON" ]; then
-		echo "$ToolchainCompile"
-	fi
+	. ../android_prebuilt_toolchains/aptess.sh
 else
 	echo "Script says: You don't have TeamVee Prebuilt Toolchains"
 	echo ""
