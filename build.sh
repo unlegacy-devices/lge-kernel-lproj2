459c070632aefa55f28ec9932246c83111715c5e
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-01-15 08:30:54 -0200

    zip-creator: Update to new simple script inside update-binary
    And massive build.sh update
    
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- build.sh
+++ build.sh
@@ -25,26 +25,9 @@ if [ "$coloroutput" == "ON" ]; then
 	bldcya=${txtbld}$(tput setaf 6) # cyan
 	bldwhi=${txtbld}$(tput setaf 7) # white
 	coloroutputzip="--color=auto"
-fi
-if [ "$coloroutput" == "OFF" ]; then
+else
 	unset txtbld bldred bldgrn bldyel bldblu bldmag bldcya bldwhi
 	coloroutputzip="--color=never"
-fi
-}
-
-setcustomcoloroutput() {
-if [ "$coloroutput" == "ON" ]; then
-	coloroutput="OFF"
-else
-	coloroutput="ON"
-fi
-}
-
-setcustombuildoutput() {
-if [ "$buildoutput" == "ON" ]; then
-	buildoutput="OFF"
-else
-	buildoutput="ON"
 fi
 }
 
@@ -53,7 +36,7 @@ fi
 # Clean - Start
 
 cleanzip() {
-rm -rf zip-creator/*.zip zip-creator/zImage zip-creator/system/lib/modules/*.ko
+rm -rf zip-creator/*.zip
 cleanzipcheck="Done"
 unset zippackagecheck
 }
@@ -101,7 +84,7 @@ if ! [ "$errorchoice" == "ON" ]; then
 	echo "$choice - $name $variant"; make $defconfig &> /dev/null | echo "Setting..."
 	maindevicecheck="ON"
 	unset cleankernelcheck
-	zipfile="$customkernel-$name-$variant-$daytime.zip"
+	zipfile="$customkernel-$name-$variant-$release.zip"
 fi
 }
 
@@ -137,20 +120,20 @@ fi
 # Build Process - Start
 
 buildprocess() {
-START=$(date +"%s")
-NR_CPUS=$(grep -c ^processor /proc/cpuinfo)
-if [ "$NR_CPUS" -le "2" ]; then
-	NR_CPUS="4"
-fi
+NR_CPUS=$(($(grep -c ^processor /proc/cpuinfo) + $(grep -c ^processor /proc/cpuinfo)))
+
 echo "${bldblu}Building $customkernel with $NR_CPUS jobs at once${txtrst}"
+
 rm -rf arch/$ARCH/boot/zImage
-if [ "$buildoutput" == "ON" ]; then
-	make -j${NR_CPUS}
-else
-	make -j${NR_CPUS} &>/dev/null | loop
+
+START=$(date +"%s")
+if [ "$buildoutput" == "ON" ]
+	then make -j${NR_CPUS}
+	else make -j${NR_CPUS} &>/dev/null | loop
 fi
 END=$(date +"%s")
 BUILDTIME=$(($END - $START))
+
 if [ -f arch/$ARCH/boot/zImage ]; then
 	buildprocesscheck="Done"
 	unset cleankernelcheck
@@ -164,8 +147,8 @@ LEND=$(date +"%s")
 LBUILDTIME=$(($LEND - $START))
 echo -ne "\r\033[K"
 echo -ne "${bldgrn}Build Time: $(($LBUILDTIME / 60)) minutes and $(($LBUILDTIME % 60)) seconds.${txtrst}"
-sleep 1
 if ! [ -f arch/$ARCH/boot/zImage ]; then
+	sleep 1
 	loop
 fi
 }
@@ -175,34 +158,19 @@ fi
 # Zip Process - Start
 
 zippackage() {
-# Variables
-zipdaytime=$(date +%d" "%m" "%Y)
-fileupdaterscript="zip-creator/META-INF/com/google/android/updater-script"
-fileupdaterscripttemp="zip-creator/META-INF/com/google/android/updater-script-temp"
-stockname="L5"
-stockvariant="NFC"
-
-# Updater-script
-sed "s/$stockname/$name/; s/$stockvariant/$variant/" $fileupdaterscript > $fileupdaterscripttemp
-mv $fileupdaterscripttemp $fileupdaterscript
-# Updater-script - Time
-sed "s/Today/Release Day $zipdaytime/" $fileupdaterscript > $fileupdaterscripttemp
-mv $fileupdaterscripttemp $fileupdaterscript
-
+echo "${name}" >> zip-creator/device.prop
+echo "${variant}" >> zip-creator/device.prop
+echo "${release}" >> zip-creator/device.prop
 
 cp arch/$ARCH/boot/zImage zip-creator
-find . -name *.ko | xargs cp -a --target-directory=zip-creator/system/lib/modules/ &> /dev/null
+mkdir zip-creator/modules
+find . -name *.ko | xargs cp -a --target-directory=zip-creator/modules/ &> /dev/null
 
 cd zip-creator
-zip -r $zipfile * -x */.gitignore *.zip &> /dev/null
+zip -r $zipfile * -x .gitignore *.zip &> /dev/null
 cd ..
 
-# Updater-script - Time
-sed "s/Release Day $zipdaytime/Today/" $fileupdaterscript > $fileupdaterscripttemp
-mv $fileupdaterscripttemp $fileupdaterscript
-# Updater-script
-sed "s/$name/$stockname/; s/$variant/$stockvariant/" $fileupdaterscript > $fileupdaterscripttemp
-mv $fileupdaterscripttemp $fileupdaterscript
+rm -rf zip-creator/zImage zip-creator/modules zip-creator/device.prop
 
 zippackagecheck="Done"
 unset cleanzipcheck
@@ -226,28 +194,18 @@ esac
 
 # ADB - End
 
-# Misc - Start
-
-buildtimemisc() {
-if ! [ "$BUILDTIME" == "" ]; then
-	echo "${bldgrn}Build Time: $(($BUILDTIME / 60)) minutes and $(($BUILDTIME % 60)) seconds.${txtrst}"
-fi
-}
-
-zippackagemisc() {
-echo "${bldyel}Zip Saved to zip-creator/$zipfile${txtrst}"
-}
-
-# Misc - End
-
 # Menu - Start
 
 buildsh() {
+
 kernelversion=`cat Makefile | grep VERSION | cut -c 11- | head -1`
 kernelpatchlevel=`cat Makefile | grep PATCHLEVEL | cut -c 14- | head -1`
 kernelsublevel=`cat Makefile | grep SUBLEVEL | cut -c 12- | head -1`
 kernelname=`cat Makefile | grep NAME | cut -c 8- | head -1`
-clear
+if ! [ "$buildprocesscheck" == "Something goes wrong" ]
+	then echo -e "\033c\c"
+	else clear
+fi
 echo "Simple Linux Kernel Build Script ($(date +%d"/"%m"/"%Y))"
 echo "$customkernel $kernelversion.$kernelpatchlevel.$kernelsublevel - $kernelname"
 echo
@@ -276,7 +234,9 @@ echo "-${bldblu}Test Menu:${txtrst}-"
 if [ -f zip-creator/$zipfile ]; then echo "7) Copy to device - Via Adb"; fi
 echo "8) Reboot device to recovery"
 echo "-${bldmag}Status:${txtrst}-"
-buildtimemisc
+if ! [ "$BUILDTIME" == "" ]; then
+	echo "${bldgrn}Build Time: $(($BUILDTIME / 60)) minutes and $(($BUILDTIME % 60)) seconds.${txtrst}"
+fi
 if [[ "$maindevicecheck" == "" || "$CROSS_COMPILE" == "" ]]; then
 	if [ -f arch/$ARCH/boot/zImage ]; then
 		echo "${bldblu}You have old Kernel build!${txtrst}"
@@ -284,7 +244,7 @@ if [[ "$maindevicecheck" == "" || "$CROSS_COMPILE" == "" ]]; then
 	fi
 fi
 if [ -f zip-creator/$zipfile ]; then
-	zippackagemisc
+	echo "${bldyel}Zip Saved to zip-creator/$zipfile${txtrst}"
 elif [ `ls zip-creator/*.zip 2>/dev/null | wc -l` -ge 2 ]; then
 	echo "${bldblu}You have old Zips Saved on zip-creator folder!${txtrst}"
 	ls zip-creator/*.zip ${coloroutputzip}
@@ -308,34 +268,30 @@ echo "g) Git Gui | k) GitK | s) Git Push | l) Git Pull"
 echo "q) Quit"
 read -n 1 -p "${txtbld}Choice: ${txtrst}" -s x
 case $x in
-	1) echo "$x - Cleaning Zips"; cleanzip; buildsh;;
-	2) echo "$x - Cleaning $customkernel"; cleankernel; buildsh;;
-	3) echo "$x - Device choice"; maindevice; buildsh;;
-	4) echo "$x - Toolchain choice"; maintoolchain; buildsh;;
+	1) echo "$x - Cleaning Zips"; cleanzip;;
+	2) echo "$x - Cleaning $customkernel"; cleankernel;;
+	3) echo "$x - Device choice"; maindevice;;
+	4) echo "$x - Toolchain choice"; maintoolchain;;
 	5) if [ -f .config ]; then
-		echo "$x - Building $customkernel"; buildprocess; zippackage; buildsh
-	else
-		echo "$x - This option is not valid"; sleep .5; buildsh
+		echo "$x - Building $customkernel"; buildprocess; zippackage
 	fi;;
-	6) if [ -f arch/$ARCH/boot/zImage ]; then
-		echo "$x - Ziping $customkernel"; zippackage; buildsh
-	else
-		echo "$x - This option is not valid"; sleep .5; buildsh
+	6) if ! [ "$maindevicecheck" == "" ]; then
+		if [ -f arch/$ARCH/boot/zImage ]; then
+			echo "$x - Ziping $customkernel"; zippackage
+		fi
 	fi;;
 	7) if [ -f zip-creator/$zipfile ]; then
-		echo "$x - Coping $customkernel"; adbcopy; buildsh
-	else
-		echo "$x - This option is not valid"; sleep .5; buildsh
+		echo "$x - Coping $customkernel"; adbcopy
 	fi;;
-	8) echo "$x - Rebooting to Recovery..."; adb reboot recovery; buildsh;;
-	c) setcustomcoloroutput; customcoloroutput; buildsh;;
-	o) setcustombuildoutput; buildsh;;
-	q) echo "Ok, Bye!"; unset zippackagecheck;;
-	g) echo "Opening Git Gui"; git gui; buildsh;;
-	k) echo "Opening GitK"; gitk; buildsh;;
-	s) echo "Pushing to local repo"; git push; buildsh;;
-	l) echo "Pulling to local repo"; git pull; buildsh;;
-	*) echo "$x - This option is not valid"; sleep .5; buildsh;;
+	8) echo "$x - Rebooting to Recovery..."; adb reboot recovery;;
+	c) if [ "$coloroutput" == "ON" ]; then coloroutput="OFF"; else coloroutput="ON"; fi; customcoloroutput;;
+	o) if [ "$buildoutput" == "ON" ]; then buildoutput="OFF"; else buildoutput="ON"; fi;;
+	q) echo "Ok, Bye!"; unset zippackagecheck; break;;
+	g) echo "Opening Git Gui"; git gui;;
+	k) echo "Opening GitK"; gitk;;
+	s) echo "Pushing to local repo"; git push;;
+	l) echo "Pulling to local repo"; git pull;;
+	*) echo "$x - This option is not valid"; sleep .5;;
 esac
 }
 
@@ -343,56 +299,24 @@ esac
 
 # The core of script is here!
 
-if [ -e build.sh ]; then
+if [ ! "$BASH_VERSION" ] ; then
+	echo "Please do not use sh to run this script, just use ./build.sh"
+elif [ -e build.sh ]; then
 	customkernel=CAFKernel
 	export ARCH=arm
-	daytime=$(date +%d""%m""%Y)
-	zipfile="$customkernel-$name-$variant-$daytime.zip"
+	release=$(date +%d""%m""%Y)
+	zipfile="$customkernel-$name-$variant-$release.zip"
 
-	if [ -f zip-creator/*.zip ]; then
-		unset cleanzipcheck
-	else
-		cleanzipcheck="Done"
-	fi
-
-	if [ -f .config ]; then
-		unset cleankernelcheck
-	else
-		cleankernelcheck="Done"
-	fi
-
-	if [ "$coloroutput" == "" ]; then
-		coloroutput="ON"
-	fi
-
-	if [ "$buildoutput" == "" ]; then
-		buildoutput="ON"
-	fi
+	if [ -f zip-creator/*.zip ]; then unset cleanzipcheck; else cleanzipcheck="Done"; fi
+	if [ -f .config ]; then unset cleankernelcheck; else cleankernelcheck="Done"; fi
+	if [ "$coloroutput" == "" ]; then coloroutput="ON"; fi
+	if [ "$buildoutput" == "" ]; then buildoutput="ON"; fi
 
 	customcoloroutput
 
-	if [[ "$1" = "--help" || "$1" = "-h" ]]; then
-		echo "Simple Linux Kernel Build Script"
-		echo "Use: . build.sh [OPTION]"
-		echo
-		echo "  -l, --live           use this for fast building without menu"
-		echo "  -C, --clean          use this for fast cleaning (Zip/Kernel)"
-		echo
-		echo "This is an open source script, feel free to use, edit and share it."
-		echo "By default this have a menu to interactive with kernel building."
-		echo "And for complete usage download:"
-		echo "https://github.com/TeamVee/android_prebuilt_toolchains"
-		echo "to same folder of kernel for fast toolchain select."
-	elif [[ "$1" = "--live" || "$1" = "-l" ]]; then
-		echo "--Choose a Device--"
-		maindevice
-		echo "--Choose a Toolchain--"
-		maintoolchain; buildprocess; buildtimemisc; zippackage; zippackagemisc
-	elif [[ "$1" = "--clean" || "$1" = "-C" ]]; then
-		cleanzip; cleankernel
-	else
-		buildsh
-	fi
+	while true
+		do buildsh
+	done
 else
 	echo
 	echo "Ensure you run this file from the SAME folder as where it was,"
